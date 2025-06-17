e5fa6fbd1d8371a76fd488708a64ed57f4fb0c54
Jorge Martinez <jmp@marpanet.com>
2025-06-16 23:33:54 -0600

    agregar usuario

--- public/script.js
+++ public/script.js
@@ -15,6 +15,8 @@ document.getElementById("loginForm")?.addEventListener("submit", async (e) => {
   if (res.ok) {
     localStorage.setItem("isAdmin", data.is_admin);
     localStorage.setItem("loggedIn", true);
+    localStorage.setItem("username", data.username);
+
 
     // 🔄 Ir siempre al dashboard
     window.location.href = "/dashboard";
