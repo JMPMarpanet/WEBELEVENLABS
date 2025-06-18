1b7e0a3bb14f0f5c0c5a0d353ccfd458c6dc2109
Jorge Martinez <jmp@marpanet.com>
2025-06-18 16:53:06 -0600

    Prueba 2

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ import { fileURLToPath } from "url";
 import { createClient } from "@supabase/supabase-js";
 import bcrypt from "bcrypt";
 
-
-//Prueba
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
